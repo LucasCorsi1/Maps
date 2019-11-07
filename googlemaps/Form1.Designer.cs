9e45c595e00414bac0bbd980b5345ffb0103f434
LUCAS CORSI <2015200566@unibrasil.com.br>
2019-11-07 19:44:24 -0300

    Iniciado

--- googlemaps/Form1.Designer.cs
+++ googlemaps/Form1.Designer.cs
@@ -36,17 +36,16 @@
             this.buttonclean = new System.Windows.Forms.Button();
             this.buttonmaker = new System.Windows.Forms.Button();
             this.buttonroute = new System.Windows.Forms.Button();
-            this.numAltitude = new System.Windows.Forms.NumericUpDown();
-            this.numLongitude = new System.Windows.Forms.NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)(this.numAltitude)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numLongitude)).BeginInit();
+            this.textBoxLongitude = new System.Windows.Forms.TextBox();
+            this.textBoxLatitude = new System.Windows.Forms.TextBox();
+            this.labelKm = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCarregar
             // 
             this.btnCarregar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCarregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCarregar.Location = new System.Drawing.Point(826, 98);
+            this.btnCarregar.Location = new System.Drawing.Point(820, 98);
             this.btnCarregar.Name = "btnCarregar";
             this.btnCarregar.Size = new System.Drawing.Size(112, 28);
             this.btnCarregar.TabIndex = 6;
@@ -79,15 +78,16 @@
             this.Map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.Map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.Map.ShowTileGridLines = false;
-            this.Map.Size = new System.Drawing.Size(820, 739);
+            this.Map.Size = new System.Drawing.Size(814, 739);
             this.Map.TabIndex = 8;
             this.Map.Zoom = 0D;
+            this.Map.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Map_MouseClick);
             // 
             // splitter1
             // 
             this.splitter1.Location = new System.Drawing.Point(0, 0);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(820, 739);
+            this.splitter1.Size = new System.Drawing.Size(814, 739);
             this.splitter1.TabIndex = 7;
             this.splitter1.TabStop = false;
             // 
@@ -95,7 +95,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(1011, 26);
+            this.label1.Location = new System.Drawing.Point(1011, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 20);
             this.label1.TabIndex = 9;
@@ -105,7 +105,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(1011, 55);
+            this.label2.Location = new System.Drawing.Point(1011, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 20);
             this.label2.TabIndex = 10;
@@ -115,7 +115,7 @@
             // 
             this.buttonclean.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonclean.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonclean.Location = new System.Drawing.Point(845, 166);
+            this.buttonclean.Location = new System.Drawing.Point(820, 166);
             this.buttonclean.Name = "buttonclean";
             this.buttonclean.Size = new System.Drawing.Size(201, 28);
             this.buttonclean.TabIndex = 11;
@@ -127,7 +127,7 @@
             // 
             this.buttonmaker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonmaker.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonmaker.Location = new System.Drawing.Point(944, 98);
+            this.buttonmaker.Location = new System.Drawing.Point(938, 98);
             this.buttonmaker.Name = "buttonmaker";
             this.buttonmaker.Size = new System.Drawing.Size(112, 28);
             this.buttonmaker.TabIndex = 12;
@@ -139,7 +139,7 @@
             // 
             this.buttonroute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonroute.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonroute.Location = new System.Drawing.Point(826, 132);
+            this.buttonroute.Location = new System.Drawing.Point(820, 132);
             this.buttonroute.Name = "buttonroute";
             this.buttonroute.Size = new System.Drawing.Size(112, 28);
             this.buttonroute.TabIndex = 13;
@@ -147,61 +147,42 @@
             this.buttonroute.UseVisualStyleBackColor = true;
             this.buttonroute.Click += new System.EventHandler(this.buttonroute_Click);
             // 
-            // numAltitude
+            // textBoxLongitude
             // 
-            this.numAltitude.DecimalPlaces = 9;
-            this.numAltitude.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numAltitude.Location = new System.Drawing.Point(826, 24);
-            this.numAltitude.Maximum = new decimal(new int[] {
-            -727379969,
-            232,
-            0,
-            0});
-            this.numAltitude.Minimum = new decimal(new int[] {
-            1241513983,
-            370409800,
-            542101,
-            -2147483648});
-            this.numAltitude.Name = "numAltitude";
-            this.numAltitude.Size = new System.Drawing.Size(179, 26);
-            this.numAltitude.TabIndex = 15;
-            this.numAltitude.Value = new decimal(new int[] {
-            25442026,
-            0,
-            0,
-            -2147090432});
+            this.textBoxLongitude.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLongitude.Location = new System.Drawing.Point(818, 20);
+            this.textBoxLongitude.Name = "textBoxLongitude";
+            this.textBoxLongitude.Size = new System.Drawing.Size(187, 26);
+            this.textBoxLongitude.TabIndex = 17;
+            this.textBoxLongitude.Text = "-49,238707";
             // 
-            // numLongitude
+            // textBoxLatitude
             // 
-            this.numLongitude.DecimalPlaces = 9;
-            this.numLongitude.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numLongitude.Location = new System.Drawing.Point(826, 53);
-            this.numLongitude.Maximum = new decimal(new int[] {
-            -727379969,
-            232,
-            0,
-            0});
-            this.numLongitude.Minimum = new decimal(new int[] {
-            1241513983,
-            370409800,
-            542101,
-            -2147483648});
-            this.numLongitude.Name = "numLongitude";
-            this.numLongitude.Size = new System.Drawing.Size(179, 26);
-            this.numLongitude.TabIndex = 16;
-            this.numLongitude.Value = new decimal(new int[] {
-            49238707,
-            0,
-            0,
-            -2147090432});
+            this.textBoxLatitude.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLatitude.Location = new System.Drawing.Point(818, 57);
+            this.textBoxLatitude.Name = "textBoxLatitude";
+            this.textBoxLatitude.Size = new System.Drawing.Size(187, 26);
+            this.textBoxLatitude.TabIndex = 18;
+            this.textBoxLatitude.Text = "-25,442026";
+            // 
+            // labelKm
+            // 
+            this.labelKm.AutoSize = true;
+            this.labelKm.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKm.Location = new System.Drawing.Point(820, 214);
+            this.labelKm.Name = "labelKm";
+            this.labelKm.Size = new System.Drawing.Size(40, 24);
+            this.labelKm.TabIndex = 19;
+            this.labelKm.Text = "Km";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1129, 739);
-            this.Controls.Add(this.numLongitude);
-            this.Controls.Add(this.numAltitude);
+            this.ClientSize = new System.Drawing.Size(1123, 739);
+            this.Controls.Add(this.labelKm);
+            this.Controls.Add(this.textBoxLatitude);
+            this.Controls.Add(this.textBoxLongitude);
             this.Controls.Add(this.buttonroute);
             this.Controls.Add(this.buttonmaker);
             this.Controls.Add(this.buttonclean);
@@ -212,8 +193,6 @@
             this.Controls.Add(this.btnCarregar);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.numAltitude)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numLongitude)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,8 +207,9 @@
         private System.Windows.Forms.Button buttonclean;
         private System.Windows.Forms.Button buttonmaker;
         private System.Windows.Forms.Button buttonroute;
-        private System.Windows.Forms.NumericUpDown numAltitude;
-        private System.Windows.Forms.NumericUpDown numLongitude;
+        private System.Windows.Forms.TextBox textBoxLongitude;
+        private System.Windows.Forms.TextBox textBoxLatitude;
+        private System.Windows.Forms.Label labelKm;
     }
 }
 
--- googlemaps/Form1.cs
+++ googlemaps/Form1.cs
@@ -19,64 +19,57 @@ namespace googlemaps
         public Form1()
         {
             InitializeComponent();
+            points = new List<PointLatLng>();
             GMapProviders.GoogleMap.ApiKey = @"AIzaSyA85W4s3A3xP0vCc_5L-ZIQ8gW7VUlCi8Y"; // KEY  **não compartilhar por favor
             Map.MapProvider = GMapProviders.GoogleMap;
-            Map.Zoom = 15;
-            points = new List<PointLatLng>();
-
+            Map.DragButton = MouseButtons.Left;
+            Map.SetPositionByKeywords("Curitiba,Parana, Brazil");
+            Map.MinZoom = 3;
+            Map.MaxZoom = 15;
+            Map.Zoom = 12;
 
         }
 
         private void btnCarregar_Click(object sender, EventArgs e)
         {
-            if (numAltitude.Value != null & numLongitude.Value != null)
-            {
-                Map.DragButton = MouseButtons.Left;
-
-
-                altitude = Convert.ToDouble(numAltitude.Value);
-                longitude = Convert.ToDouble(numLongitude.Value);
-
-                Map.Position = new PointLatLng(altitude, longitude);
-
-                Map.MinZoom = 3;
-                Map.MaxZoom = 15;
-                Map.Zoom = 15;
-
-            }
+            altitude = Convert.ToDouble(textBoxLatitude.Text);
+            longitude = Convert.ToDouble(textBoxLongitude.Text);
+            Map.Position = new PointLatLng(altitude, longitude);
+            Map.Zoom = 15;
         }
 
         private void buttonmaker_Click(object sender, EventArgs e)
         {
-
             points.Add(new PointLatLng(altitude, longitude));
 
             PointLatLng point = new PointLatLng(altitude, longitude);
-            GMarkerGoogle gMarker = new GMarkerGoogle(point, GMarkerGoogleType.blue_dot);
 
-            overlay.Markers.Add(gMarker);
-
-            Map.Overlays.Add(overlay);
+            loadMap(point);
+            AddMaker(point);
+            RefreshMap();
 
         }
 
         private void buttonroute_Click(object sender, EventArgs e)
         {
+            // marcar os pontos de rotas mouse direito ou botao
+
+
             MapRoute route0 = GoogleMapProvider.Instance.GetRoute(points[0], points[1], false, false, 12);
             MapRoute route1 = GoogleMapProvider.Instance.GetRoute(points[1], points[2], false, false, 12);
             MapRoute route2 = GoogleMapProvider.Instance.GetRoute(points[2], points[3], false, false, 12);
 
             GMapRoute rt1 = new GMapRoute(route0.Points, "My Route1")
             {
-                Stroke = new Pen(Color.Cyan , 5)
+                Stroke = new Pen(Color.Cyan, 5)
             };
             GMapRoute rt2 = new GMapRoute(route1.Points, "My Route2")
             {
-                Stroke = new Pen(Color.Red , 5)
+                Stroke = new Pen(Color.Red, 5)
             };
             GMapRoute rt3 = new GMapRoute(route2.Points, "My Route3")
             {
-                Stroke = new Pen(Color.Yellow,5)
+                Stroke = new Pen(Color.Yellow, 5)
             };
 
             GMapOverlay routes = new GMapOverlay("Routes");
@@ -86,17 +79,78 @@ namespace googlemaps
 
             Map.Overlays.Add(routes);
 
+            double total = Math.Round(route0.Distance + route1.Distance + route2.Distance, 3);
 
+            labelKm.Text = "Distancia Total :  " + total.ToString() + " Km ";
+            RefreshMap();
+ 
         }
-
-
 
         private void buttonclean_Click(object sender, EventArgs e)
         {
-            points.Clear();
-            Map.Overlays.Remove(overlay);
+            if (Map.Overlays.Count > 0)
+            {
+                points.Clear();
+                Map.Overlays.Remove(overlay);
+                Map.Overlays.RemoveAt(0);
+                Map.Refresh();
+                labelKm.Text = "Km";
+                RefreshMap();
+            }
 
         }
+
+
+        private void RefreshMap()
+        {
+            Map.Zoom--;
+            Map.Zoom++;
+        }
+
+
+        // marca com o botão do mouse
+
+        private void Map_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+
+                PointLatLng Point = Map.FromLocalToLatLng(e.X, e.Y);
+                altitude = Point.Lat;
+                longitude = Point.Lng;
+
+                textBoxLatitude.Text = altitude.ToString();
+                textBoxLongitude.Text = longitude.ToString();
+
+                AddMaker(Point);
+                points.Add(Point);
+                RefreshMap();
+
+            }
+
+        }
+
+
+        private void loadMap(PointLatLng point)
+        {
+            Map.Position = point;
+        }
+
+        private void AddMaker(PointLatLng pointLatLng, GMarkerGoogleType gMarker = GMarkerGoogleType.arrow)
+        {
+
+            GMarkerGoogle Marker = new GMarkerGoogle(pointLatLng, gMarker)
+            {
+                ToolTipText = $"Latitude: {Map.Position.Lat} + Longitude: {Map.Position.Lng}"
+            };
+
+            overlay.Markers.Add(Marker);
+
+            Map.Overlays.Add(overlay);
+            RefreshMap();
+
+        }
+
     }
 
 }
